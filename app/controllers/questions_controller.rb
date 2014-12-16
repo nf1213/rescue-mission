bff1f659dff06a3fef02b507cc88c3a49ad77495
Nicole <nfelch121394@gmail.com>
2014-12-15 20:19:18 -0500

    Style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,9 @@
 class QuestionsController < ApplicationController
 
+  def current_user_id
+    1
+  end
+
   def index
     @questions = Question.all.order(created_at: :desc)
   end
@@ -14,9 +18,10 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.new(title: params[:question][:title], description: params[:question][:description], user_id: 1)
+    @question = Question.new(question_params)
+    @question.user_id = current_user_id
     if @question.save
-      redirect_to @question, notice: 'Issue was successfully created.'
+      redirect_to @question, notice: 'Question was successfully created.'
     else
       render :new
     end
@@ -35,6 +40,6 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-
+    params.require(:question).permit(:title, :description)
   end
 end
